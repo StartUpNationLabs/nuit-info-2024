7ddc9ff20c345f251545c38c1af9d9f73e177242
BedinoTom <tom.bedino@etu.univ-cotedazur.fr>
2024-12-06 00:47:33 +0100

    feat(header): add header to website

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,22 @@
-import { Grid, Typography } from "@mui/material";
-import { Suspense } from "react";
-import Scene from "./components/3d/Scene.tsx";
-import { Canvas } from "@react-three/fiber";
-import { LoadingScreen } from "./LoadingScreen.tsx";
-import { ToastContainer } from "react-toastify";
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import "react-toastify/dist/ReactToastify.css";
 
+import { Header } from './Header';
+import { HumanEarth } from './HumanEarth';
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Header />}>
+        <Route index element={<HumanEarth />} />
+      </Route>
+    )
+  );
+
 export function App() {
+
     return (
-        <>
-            <Grid>
-                <Canvas
-                    gl={{ alpha: true, stencil: false, antialias: true, depth: true }}
-                    style={{
-                        height: "100vh",
-                    }}
-                    dpr={1}
-                    camera={{
-                        near: 0.01,
-                        far: 100000000,
-                    }}
-                >
-                    <Suspense fallback={<LoadingScreen />}>
-                        <Scene />
-                    </Suspense>
-                </Canvas>
-            </Grid>
-            <ToastContainer position="bottom-center" />
-            <Typography
-                style={{
-                    position: "absolute",
-                    bottom: 0,
-                    right: 0,
-                    margin: "0.5em",
-                    color: "white",
-                    fontSize: "0.5em",
-                    opacity: 0.5,
-                }}
-            >
-                v
-                {import.meta.env.VITE_APP_VERSION ||
-                    import.meta.env.PACKAGE_VERSION ||
-                    "0.0.0"}
-            </Typography>
-        </>
+      <>
+        <RouterProvider router={router}/>
+      </>
     );
-}
+  }
