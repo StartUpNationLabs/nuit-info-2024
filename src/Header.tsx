f5261c8284cc9a2e5b3d9c656fa317b7c8a210bc
BedinoTom <tom.bedino@etu.univ-cotedazur.fr>
2024-12-06 01:02:28 +0100

    feat(menu): add menu toolbar

--- src/Header.tsx
+++ src/Header.tsx
@@ -5,8 +5,21 @@ import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import { Outlet } from "react-router-dom";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { useState } from "react";
 
 export const Header = () => {
+    const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenuOpen = (event: any) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
     return (
         <><AppBar position="static">
             <Toolbar>
@@ -18,6 +31,7 @@ export const Header = () => {
                     color="inherit"
                     aria-label="menu"
                     sx={{ mr: 2 }}
+                    onClick={handleMenuOpen}
                 >
                     {/*This is a simple Menu
       Icon wrapped in Icon */}
@@ -25,13 +39,21 @@ export const Header = () => {
                 </IconButton>
                 {/* The Typography component applies
          default font weights and sizes */}
-
+                <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleMenuClose}
+        >
+          <MenuItem onClick={handleMenuClose}>Option 1</MenuItem>
+          <MenuItem onClick={handleMenuClose}>Option 2</MenuItem>
+          <MenuItem onClick={handleMenuClose}>Option 3</MenuItem>
+        </Menu>
                 <Typography
                     variant="h6"
                     component="div"
                     sx={{ flexGrow: 1 }}
                 >
-                    GeeksforGeeks Header
+                    Human'O
                 </Typography>
                 <Button color="inherit">Login</Button>
             </Toolbar>
